2f582c48b7867514cc014f2e149604ef79226c4b
Paweł Wilczek <5646246+beowoolf@users.noreply.github.com>
2023-12-10 11:16:01 +0100

    Added new version of endpoint to fetch job offers from nofluffjobs.com

--- jobs/nofluffjobs/index.php
+++ jobs/nofluffjobs/index.php
@@ -1,76 +1,72 @@
 <?php
 
-header('Content-Type: application/json');
-
-// Wyłączanie raportowania błędów XML
-libxml_use_internal_errors(true);
-
-// URL do strony do parsowania
-$url = 'https://nofluffjobs.com/pl';
-
-// Inicjalizacja cURL
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-// Wykonanie zapytania cURL
-$response = curl_exec($ch);
-
-// Sprawdzenie, czy zapytanie się powiodło
-if ($response === false) {
-    echo json_encode(array("success" => false, "errorMsg" => "Błąd podczas pobierania zawartości strony."));
-} else {
-    // Tworzenie obiektu DOMDocument
-    $doc = new DOMDocument();
-
-    // Wczytywanie kodu HTML
-    if ($doc->loadHTML($response)) {
-        // Tworzenie obiektu DOMXPath
-        $xpath = new DOMXPath($doc);
-
-        // Wyrażenie XPath
-        $xpathExpression = '//*[@id="serverApp-state"]/text()';
-
-        // Wyszukiwanie tekstu za pomocą XPath
-        $textNodeList = $xpath->query($xpathExpression);
-
-        // Sprawdzanie, czy znaleziono węzły
-        if ($textNodeList->length > 0) {
-            $text = $textNodeList->item(0)->nodeValue;
-            $text = str_replace("&q;", '"', $text);
-            $arr = json_decode($text, true);
-            $new_arr = array(
-                "joboffersAutocompleteConfig" => $arr["/joboffers/autocomplete/config?salaryCurrency=PLN&a;salaryPeriod=month&a;region=pl"],
-                //"posting" => $arr["/posting?limited=6&a;salaryCurrency=PLN&a;salaryPeriod=month&a;region=pl"],
-                "joboffersOfTheDay" => $arr["/joboffers/oftheday?salaryCurrency=PLN&a;salaryPeriod=month&a;region=pl"],
-                //"offersCategorized" => $arr["OFFERS_CATEGORIZED"],
-                "offersOfTheDay" => $arr["OFFERS_OF_THE_DAY"]
-            );
-            unset($arr["USER_COUNTRY"]);//X
-            unset($arr["assets/environments/prod.json?salaryCurrency=PLN&a;salaryPeriod=month&a;region=pl"]);//X
-            unset($arr["translations_pl-PL"]);//X
-            unset($arr["/feature?salaryCurrency=PLN&a;salaryPeriod=month&a;region=pl"]);//X
-            unset($arr["STORE_KEY"]);//X
-            unset($arr["/joboffers/count?salaryCurrency=PLN&a;salaryPeriod=month&a;region=pl"]);//X
-            unset($arr["assigned on server"]);//X
-            unset($arr["assets/mock/country-codes-list.json?salaryCurrency=PLN&a;salaryPeriod=month&a;region=pl"]);//X
-            unset($arr["/joboffers/autocomplete/config?salaryCurrency=PLN&a;salaryPeriod=month&a;region=pl"]);//FILTR
-            unset($arr["/posting?limited=6&a;salaryCurrency=PLN&a;salaryPeriod=month&a;region=pl"]);//OK
-            unset($arr["/joboffers/oftheday?salaryCurrency=PLN&a;salaryPeriod=month&a;region=pl"]);//OK
-            unset($arr["OFFERS_CATEGORIZED"]);//OK
-            unset($arr["OFFERS_OF_THE_DAY"]);//OK
-            echo json_encode(array("success" => true, "response" => $new_arr), JSON_UNESCAPED_SLASHES);
-        } else {
-            echo json_encode(array("success" => false, "errorMsg" => "Nie znaleziono tekstu odpowiadającego wyrażeniu XPath."));
-        }
+function getJobs($page) {
+    $curl = curl_init();
+    
+    curl_setopt_array($curl, [
+      CURLOPT_URL => "https://nofluffjobs.com/api/joboffers/main?pageTo=$page&pageSize=50&salaryCurrency=PLN&salaryPeriod=month&region=pl",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "GET",
+      CURLOPT_POSTFIELDS => "",
+    ]);
+    
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    
+    curl_close($curl);
+    
+    if ($err) {
+        return array("success" => false, "errors" => "cURL Error #$page:" . $err);
     } else {
-        echo json_encode(array("success" => false, "errorMsg" => "Błąd podczas wczytywania kodu HTML."));
+        $arr_resp = json_decode($response, true);
+        return array("success" => true, "object" => $arr_resp);
     }
-
-    // Zamykanie połączenia cURL
-    curl_close($ch);
 }
 
-// Wyłączanie obsługi błędów XML
-libxml_use_internal_errors(false);
-?>
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+try {
+    $key = 'nofluffjobs.com';
+    $file_db_path = __DIR__."/../.cache.db"; // Prepare path to SQLite database in file.
+    $db = new PDO("sqlite:$file_db_path"); // Create (connect to) SQLite database in file.
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Set error mode to exceptions.
+    $db->exec('CREATE TABLE IF NOT EXISTS cache (key TEXT PRIMARY KEY, value TEXT)');
+    $stmt = $db->query("SELECT value FROM cache WHERE key = '$key'");
+    $rows = $stmt->fetchAll(PDO::FETCH_CLASS);
+    if ((isset($_GET['refresh']) && $_GET['refresh'] === '1') || (count($rows) < 1)) {
+        $jobs = array();
+        $totalPages = 1;
+        for ($i = 1; $i <= $totalPages; $i++) {
+            $response = getJobs($i);
+            if ($response["success"] === true) {
+                $totalPages = $response["object"]["totalPages"];
+                foreach ($response["object"]["postings"] as $k => $v) {
+                    //$id = $v["id"];
+                    $job = json_encode($v, JSON_UNESCAPED_SLASHES);
+                    $jobs[] = $job;
+                }
+            } else {
+                echo(json_encode($response, JSON_UNESCAPED_SLASHES));
+                break;
+            }
+        }
+
+        $final_response = '{"success":true,"list":['.implode(",", $jobs).']}';
+
+        $db->beginTransaction();
+        $query = "INSERT OR REPLACE INTO cache (key, value) VALUES (:key, :value)";
+        $stmt = $db->prepare($query);
+        $stmt->execute(array("key" => $key, "value" => $final_response));
+        $db->commit();
+
+        echo($final_response);
+    } else echo($rows[0]->value);
+    $db = null; // Close file db connection
+} catch(PDOException $e) {
+    echo(json_encode(array("sucess" => false, "errors" => array('PDO driver error: '.mb_convert_encoding($e->getMessage().' (#'.$e->getCode().') in line: '.$e->getLine(), 'UTF-8', 'ISO-8859-2'))), JSON_UNESCAPED_SLASHES)); // Print PDOException message
+}
