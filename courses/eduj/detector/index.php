82e0b0113996668385249d9d15bd04053132c5a8
Paweł Wilczek <5646246+beowoolf@users.noreply.github.com>
2024-03-22 19:57:19 +0100

    Updated endpoint to detect changes on main page on one popular polish course video platform

--- courses/eduj/detector/index.php
+++ courses/eduj/detector/index.php
@@ -1,41 +1,6 @@
 <?php
 
-header("Content-Type: application/json; charset=UTF-8");
-
-function mapJsonObject($json) {
-    if (!$json) return "";
-    $array = json_decode($json, true);
-    $items = array();
-    foreach ($array["data"]["products"]["items"] as $key => $value) {
-        $items[] = array(
-            "id" => $value["id"],
-            "name" => $value["name"],
-            "price" => $value["price_range"],
-            "sku" => $value["sku"],
-            "discounted_price" => $value["discounted_price"],
-            "author_name" => $value["author_name"],
-            "image_url" => $value["image_url"],
-            "categories" => $value["categories"],
-            "description" => $value["description"]["html"]
-        );
-    }
-    $out = array(
-        "data" => array(
-            "products" => array(
-                "total_count" => $array["data"]["products"]["total_count"],
-                "page_info" => array(
-                    "current_page" => $array["data"]["products"]["page_info"]["current_page"],
-                    "total_pages" => $array["data"]["products"]["page_info"]["total_pages"],
-                    "page_size" => $array["data"]["products"]["page_info"]["page_size"],
-                    "items" => $items
-                )
-            )
-        )
-    );
-    return json_encode($out, JSON_UNESCAPED_SLASHES);
-}
-
-function getFromEndpoint() {
+function getFromGraphqlEndpoint() {
     $url = 'https://ecommerce.eduj.pl/graphql';
     $headers = array(
         'Accept: application/json, text/plain, */*',
@@ -52,7 +17,7 @@ function getFromEndpoint() {
         'operationName' => 'GetProducts',
         'variables' => array(
             'search' => '',
-            'pageSize' => 1000,
+            'pageSize' => 10000,
             'currentPage' => 1
         ),
         'query' => 'query GetProducts($search: String!, $pageSize: Int!, $currentPage: Int!) {
@@ -76,6 +41,7 @@ function getFromEndpoint() {
             __typename
           }
           image_url
+          author_id
           author_name
           videos_duration
           resource_amount
@@ -95,7 +61,6 @@ function getFromEndpoint() {
           discounted_price
           categories {
             id
-            __typename
           }
           price_range {
             minimum_price {
@@ -163,29 +128,263 @@ function getFromEndpoint() {
     
     if ($response === false) {
         //"cURL Error #:" . $err;
-        return "";
+        return json_encode(array(
+            "data" => array(
+                "products" => array(
+                    "items" => array()
+                )
+            )
+        ), JSON_UNESCAPED_SLASHES);
     } else {
         return $response;
     }
 }
 
-function compareJsonFiles($file1) {
-    $json1 = @file_get_contents($file1);
-    $json2 = getFromEndpoint();
+function mapJsonObject($json) {
+    if (!$json) return json_encode(array(
+        "data" => array(
+            "products" => array(
+                "items" => array()
+            )
+        )
+    ), JSON_UNESCAPED_SLASHES);
+    $array = json_decode($json, true);
+    $items = array();
+    foreach ($array["data"]["products"]["items"] as $value) {
+        $items[] = array(
+            "id" => $value["id"],
+            "name" => $value["name"],
+            "price_range" => $value["price_range"],
+            "sku" => $value["sku"],
+            "discounted_price" => $value["discounted_price"],
+            "author_id" => $value["author_id"],
+            "author_name" => $value["author_name"],
+            "image_url" => $value["image_url"],
+            "categories" => $value["categories"],
+            "description" => $value["description"]["html"]
+        );
+    }
+    $out = array(
+        "data" => array(
+            "products" => array(
+                "total_count" => $array["data"]["products"]["total_count"],
+                "page_info" => array(
+                    "current_page" => $array["data"]["products"]["page_info"]["current_page"],
+                    "total_pages" => $array["data"]["products"]["page_info"]["total_pages"],
+                    "page_size" => $array["data"]["products"]["page_info"]["page_size"]
+                ),
+                "items" => $items
+            )
+        )
+    );
+    return json_encode($out, JSON_UNESCAPED_SLASHES);
+}
+
+function mapCategories($old_arr) {
+    $new_arr = array();
+    foreach ($old_arr as $key => $value)
+        $new_arr[] = $value["id"];
+    return $new_arr;
+}
+
+function mapCourseItems($response) {
+    $arr = json_decode($response, true);
+    $courses = array();
+    foreach ($arr["data"]["products"]["items"] as $value) {
+        $courses[] = array(
+            "title" => $value["name"],
+            "price" => $value["price_range"]["maximum_price"]["regular_price"]["value"],
+            "url" => "https://eduj.pl/produkt/{$value["sku"]}",
+            /*"platform" => array(
+                "name" => "Eduj",
+                "logo" => "https://eduj.pl/assets/images/logo-eduj.svg",
+                "url" => "https://eduj.pl/",
+                "type" => "eduj",
+                "version" => "1.0.0",
+                "subscriptionMode" => false
+            ),*/
+            "discountedPrice" => $value["price_range"]["minimum_price"]["final_price"]["value"],
+            "thumbnail" => str_replace("/thumbnail.jpeg","/thumbnail_848x480.jpeg", $value["image_url"]),
+            "authorId" => $value["author_id"],
+            "categoryIds" => mapCategories($value["categories"]),
+            "description" => strip_tags($value["description"])
+        );
+    }
+    foreach ($courses as $key => $value)
+        if ($value["price"] == $value["discountedPrice"])
+            unset($courses[$key]["discountedPrice"]);
+    
+    return $courses;
+}
+
+function getLinksFromSitemap() {
+    // Ustawienia zapytania cURL
+    $url = 'https://eduj.pl/sitemap.xml';
+    $cacheTable = 'cache.db'; // Nazwa pliku bazy SQLite
+    $edujTable = 'eduj';
+    
+    // Inicjalizacja bazy SQLite
+    $db = new SQLite3($cacheTable);
+    
+    // Tworzenie tabeli "eduj", jeśli nie istnieje
+    $db->exec("CREATE TABLE IF NOT EXISTS $edujTable (url TEXT PRIMARY KEY)");
+    
+    // Inicjalizacja sesji cURL
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    
+    // Wykonaj zapytanie cURL
+    $response = curl_exec($ch);
+    $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    
+    // Zamknij sesję cURL
+    curl_close($ch);
+    
+    if ($status_code === 200 && $response !== false) {
+        // Przetwarzanie pliku XML
+        $xml = simplexml_load_string($response);
+    
+        if ($xml !== false) {
+            // Wyciągnij adresy URL z pliku sitemap.xml
+            $urls = $xml->url;
+    
+            if (count($urls) > 0) {
+                // Przygotuj listę adresów URL, które zawierają ciąg "/produkt/"
+                $filteredUrls = [];
+                
+                foreach ($urls as $url) {
+                    $urlString = (string)$url->loc;
+                    
+                    if (strpos($urlString, '/produkt/') !== false) {
+                        $filteredUrls[] = $urlString;
+                    }
+                }
+    
+                if (count($filteredUrls) > 0) {
+                    // Odczytaj aktualne adresy URL z bazy SQLite
+                    $existingUrls = [];
+                    $result = $db->query("SELECT url FROM $edujTable");
+    
+                    while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+                        $existingUrls[] = $row['url'];
+                    }
+    
+                    // Wyklucz istniejące adresy, które nie zawierają "/produkt/"
+                    $filteredUrls = array_diff($filteredUrls, $existingUrls);
+    
+                    if (count($filteredUrls) > 0) {
+                        // Dodaj nowe adresy do bazy SQLite
+                        $stmt = $db->prepare("INSERT OR REPLACE INTO $edujTable (url) VALUES (:url)");
+    
+                        foreach ($filteredUrls as $urlString) {
+                            $stmt->bindValue(':url', $urlString, SQLITE3_TEXT);
+                            $stmt->execute();
+                        }
+    
+                        error_log("Pobrano i zaktualizowano adresy z sitemap.xml w bazie SQLite.");
+                    } else {
+                        error_log("Sitemap.xml nie zawiera nowych adresów URL zawierających '/produkt/'.");
+                    }
+                } else {
+                    error_log("Sitemap.xml nie zawiera nowych adresów URL zawierających '/produkt/'.");
+                }
+    
+                // Zwróć adresy URL zapisane w bazie SQLite, które zawierają "/produkt/"
+                $result = $db->query("SELECT url FROM $edujTable");
+                $urls = [];
+    
+                while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+                    $urlString = $row['url'];
+                    
+                    if (strpos($urlString, '/produkt/') !== false) {
+                        $urls[] = $urlString;
+                    }
+                }
+    
+                return $urls;
+            } else {
+                error_log("Sitemap.xml nie zawiera adresów URL.");
+                return [];
+            }
+        } else {
+            error_log("Błąd podczas przetwarzania pliku XML.");
+    
+            // Spróbuj zwrócić zapisane adresy URL z tabeli "eduj" bazy SQLite, które zawierają "/produkt/" w formacie JSON
+            $result = $db->query("SELECT url FROM $edujTable");
+            $urls = [];
+    
+            while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+                $urlString = $row['url'];
+    
+                if (strpos($urlString, '/produkt/') !== false) {
+                    $urls[] = $urlString;
+                }
+            }
+    
+            return $urls;
+        }
+    } else {
+        // Błąd podczas pobierania sitemap.xml - logowanie i próba odczytu danych z bazy SQLite
+        error_log("Błąd podczas pobierania sitemap.xml.");
+    
+        // Spróbuj zwrócić zapisane adresy URL z tabeli "eduj" bazy SQLite, które zawierają "/produkt/" w formacie JSON
+        $result = $db->query("SELECT url FROM $edujTable");
+        $urls = [];
+    
+        while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+            $urlString = $row['url'];
+    
+            if (strpos($urlString, '/produkt/') !== false) {
+                $urls[] = $urlString;
+            }
+        }
+    
+        return $urls;
+    }
+    
+    // Zamknij połączenie z bazą SQLite
+    $db->close();
+}
+
+function extractLinksFromResponse($mapped_courses_list) {
+    $links_list = array();
+    foreach ($mapped_courses_list as $value)
+        $links_list[] = $value["url"];
+    return $links_list;
+}
+
+function getMissedLinks($mapped_courses_list) {
+    $links_from_sitemap = getLinksFromSitemap();
+    $links_from_graphql = extractLinksFromResponse($mapped_courses_list);
+    $all_unique_links = array_values(array_unique(array_merge($links_from_sitemap, $links_from_graphql)));
+    return array_values(array_diff($all_unique_links, $links_from_graphql));
+}
+
+function compareJsonFiles($json_file) {
+    $json1 = @file_get_contents($json_file);
+    $json2 = getFromGraphqlEndpoint();
     $res1 = mapJsonObject($json1);
     $res2 = mapJsonObject($json2);
     
     // Porównaj czysty kod HTML
     if ($res1 === $res2) {
-        return "up-to-date"; // Brak zmian
+        return array(
+            "courses" => array(),
+            "links" => array()
+        ); // Brak zmian
     } else {
-        file_put_contents($file1, $json2);
-        return "out-to-date"; // Znaleziono zmiany
+        file_put_contents($json_file, $json2);
+        $courses = mapCourseItems($res2);
+        return array(
+            "courses" => $courses,
+            "links" => getMissedLinks($courses)
+        ); // Znaleziono zmiany
     }
 }
 
-$file1 = 'last.json'; // Plik na dysku
+$file_with_last_response = 'last.json'; // Plik na dysku
 
-echo json_encode(compareJsonFiles($file1));
+echo json_encode(compareJsonFiles($file_with_last_response), JSON_UNESCAPED_SLASHES);
 
 ?>
